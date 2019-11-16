5d9cc15b5cf1d5836120ac00d89c14f4194a2ee1
d86leader <d86leader@mail.com>
2019-11-16 14:13:27 +0700

    move dotfiles to vimconfig

---
+++ install-dotfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s -T "${HOME}/.vim/dotfiles/bashrc"  "${HOME}/.bashrc"
+ln -s -T "${HOME}/.vim/dotfiles/inputrc" "${HOME}/.inputrc"
---
+++ install-nvim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s -T "${HOME}/.vim/vimrc" "${HOME}/.vim/init.vim"
+ln -s -T "${HOME}/.vim" "${HOME}/.config/nvim"
