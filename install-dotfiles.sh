e1951c5d6a4b71dbadb315dfa8f9dcf389d0fb05
d86leader <d86leader@gmail.com>
2021-02-24 18:32:07 +0700

    add dotfile for haskeline (ghci)

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,3 +2,4 @@
 
 ln -s -T "${HOME}/.vim/dotfiles/bashrc"  "${HOME}/.bashrc"
 ln -s -T "${HOME}/.vim/dotfiles/inputrc" "${HOME}/.inputrc"
+ln -s -T "${HOME}/.vim/dotfiles/haskeline" "${HOME}/.haskeline"
