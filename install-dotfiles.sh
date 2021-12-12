fd86636f05db4362066641cb5ed9b8cdde1819e8
d86leader <d86leader@gmail.com>
2021-12-12 16:05:14 +0700

    Add dotfiles which are not dot

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,3 +3,9 @@
 ln -s -T "${HOME}/.vim/dotfiles/bashrc"  "${HOME}/.bashrc"
 ln -s -T "${HOME}/.vim/dotfiles/inputrc" "${HOME}/.inputrc"
 ln -s -T "${HOME}/.vim/dotfiles/haskeline" "${HOME}/.haskeline"
+
+mkdir -p "${HOME}/.ghc/"
+ln -s -T "${HOME}/.vim/dotfiles/ghc/ghci.conf" "${HOME}/.ghc/ghci.conf"
+
+ln -s -T "${HOME}/.vim/dotfiles/config/mpv" "${HOME}/.config/mpv"
+ln -s -T "${HOME}/.vim/dotfiles/config/streamlink" "${HOME}/.config/streamlink"
